d0f39c4430a938819bed1c386ef50b194c7f6014
rs.drip <rs.drip@aa49b5b2-a402-11dd-98aa-2b35b7097d33>
2008-12-04 08:34:22 +0000

    Kenji	: ブロックの回転・スケジュール更新

--- プログラム/Game/Object/GameScene/Block.h
+++ プログラム/Game/Object/GameScene/Block.h
@@ -1,6 +1,20 @@
+/*******************************************************************************/
+/**
+ * @file Block.h.
+ * 
+ * @brief ブロッククラス定義.
+ *
+ * @date 2008/12/01.
+ *
+ * @version 1.00.
+ *
+ * @author Tsubasa Uragami.
+ */
+/*******************************************************************************/
 #ifndef _BLOCK_H_
 #define _BLOCK_H_
 
+/*===== インクルード ==========================================================*/
 #include	"IGameDevice.h"
 #include	"Manager/Object/ObjectManager.h"
 #include	"Object/ObjectBase.h"
@@ -8,18 +22,87 @@
 #include	"Manager/Scene/Option/Option.h"
 class		IGameDevice;
 
+
+/**
+ * @brief SPINBLOCK．
+ */
+enum SPINBLOCK
+{
+	SPINBLOCK_RIGHT = 0,
+	SPINBLOCK_LEFT,
+};
+
+/**
+ * @brief Block．
+ */
 class  Block : public ObjectBase
 {
 public:
+
+	/*=========================================================================*/
+	/**
+	 * @brief コンストラクタ.
+	 * 
+	 * @param[in] device ゲームデバイス.
+	 * @param[in] objectManager オブジェクトマネージャ.
+	 * @param[in] option ゲームオプション.
+	 * @param[in] gameSceneState ゲームシーンステート.
+	 * @param[in] blockCID 軸のブロックのID.
+	 * @param[in] blockMID サブのブロックのID.
+	 */
 	Block(IGameDevice& device, ObjectManager& objectManager, Option& option, GameSceneState& gameSceneState, int blockCID, int blockMID);
+	/*=========================================================================*/
+	/**
+	 * @brief デストラクタ.
+	 *
+	 */
 	~Block();
 
+	/*=========================================================================*/
+	/**
+	 * @brief 初期化処理.
+	 * 
+	 */
 	void Initialize();
+	/*=========================================================================*/
+	/**
+	 * @brief 終了処理.
+	 * 
+	 */
 	void Terminate();
+	/*=========================================================================*/
+	/**
+	 * @brief 終了しているかどうか.
+	 * 
+	 * @return 終了フラグ.
+	 */
 	bool IsTerminated();
+	/*=========================================================================*/
+	/**
+	 * @brief オブジェクトの描画処理.
+	 * 
+	 */
 	void RenderObject();
+	/*=========================================================================*/
+	/**
+	 * @brief オブジェクトの更新処理.
+	 * 
+	 * @param[in] frameTimer 更新タイマ.
+	 */
 	void UpdateObject(float frameTimer);
+	/*=========================================================================*/
+	/**
+	 * @brief ブロック配列の初期化.
+	 * 
+	 */
 	void InitializeMatrix();
+	/*=========================================================================*/
+	/**
+	 * @brief ブロックの回転.
+	 * 
+	 * @param[in] direction 回転方向定数.
+	 */
+	void SpinBlock(int direction);
 
 private:
 
@@ -34,16 +117,18 @@ private:
 	/** ゲームシーンステート */
 	GameSceneState m_gameSceneState;
 	/** ブロックの配列 */
-	int blockMatrix[3][3];
+	int m_blockMatrix[3][3];
 	/** ブロックのID */
-	int blockID[2];
+	int m_blockID[2];
 	/** スピード */
-	float speed;
+	float m_speed;
 
-	float x;
-	float y;
-	float size;
-	float time;
+	/** X位置 */
+	float m_x;
+	/** Y位置 */
+	float m_y;
+	/** ブロックのサイズ */
+	float m_size;
 };
 
 #endif
