5fd39098694ee6b095fdc4886220a3df525d99a8
rs.drip <rs.drip@aa49b5b2-a402-11dd-98aa-2b35b7097d33>
2008-12-04 11:15:21 +0000

    Ryoma	: ブロックスクリーン追加忘れてましたすいません去勢しますｂｙおがわ

---
+++ trunk/プログラム/Game/Object/GameScene/PuzzleScreen.cpp
@@ -0,0 +1,82 @@
+/*******************************************************************************/
+/**
+ * @file PuzzleScreen.cpp.
+ * 
+ * @brief パズルスクリーンクラスソースファイル.
+ *
+ * @date 2008/12/4.
+ *
+ * @version 1.00.
+ *
+ * @author ryoma kawasue.
+ */
+/*******************************************************************************/
+
+#include "PuzzleScreen.h"
+
+PuzzleScreen::PuzzleScreen(IGameDevice& device, ObjectManager& objectManager, Option &option, GameSceneState& gameSceneState) :
+	m_device(device), m_objectManager(objectManager), m_option(option), m_gameSceneState(gameSceneState), m_isTerminated(false),
+	m_blockManager(device, objectManager, option, gameSceneState )
+{
+}
+
+/*=============================================================================*/
+/**
+ * @brief デストラクタ.
+ *
+ */
+PuzzleScreen::~PuzzleScreen()
+{
+}
+
+/*=============================================================================*/
+/**
+ * @brief 初期化処理.
+ *
+ */
+void PuzzleScreen::Initialize()
+{
+	m_blockManager.Initialize();
+}
+
+/*=============================================================================*/
+/**
+ * @brief 終了しているかどうか.
+ *
+ */
+bool PuzzleScreen::IsTerminated()
+{
+	return m_isTerminated;
+}
+
+/*=============================================================================*/
+/**
+ * @brief オブジェクトの描画処理.
+ *
+ */
+void PuzzleScreen::RenderObject()
+{
+	m_blockManager.RenderObject();
+}
+
+/*=============================================================================*/
+/**
+ * @brief 終了処理.
+ *
+ */
+void PuzzleScreen::Terminate()
+{
+	m_isTerminated = true; 
+}
+
+/*=============================================================================*/
+/**
+ * @brief オブジェクトの更新処理.
+ *
+ */
+void PuzzleScreen::UpdateObject(float frameTimer)
+{
+	m_blockManager.UpdateObject(frameTimer);
+}
+
+/*===== EOF ===================================================================*/
