b6e05f8efb4f1051cb8ee1ff3f71d3cca232623f
rs.drip <rs.drip@aa49b5b2-a402-11dd-98aa-2b35b7097d33>
2008-12-11 03:24:25 +0000

    Ryosuke	: アバタークラスとキャラクタースクリーンクラスを追加

---
+++ trunk/プログラム/Game/Object/GameScene/Avatar.h
@@ -0,0 +1,98 @@
+/*******************************************************************************/
+/**
+ * @file Avatar.h.
+ * 
+ * @brief アバタークラスヘッダ.
+ *
+ * @date 2008/12/11.
+ *
+ * @version 1.00.
+ *
+ * @author Ryosuke Ogawa.
+ */
+/*******************************************************************************/
+#ifndef _AVATAR_H_
+#define _AVATAR_H_
+
+#include	"IGameDevice.h"
+#include	"Manager/Object/ObjectManager.h"
+#include	"Object/ObjectBase.h"
+#include	"Scene/GameSceneState.h"
+#include	"Manager/Scene/Option/Option.h"
+
+
+/**
+ * @brief Avater．
+ */
+class Avatar : public ObjectBase
+{
+public:
+
+	/*=========================================================================*/
+	/**
+	 * @brief コンストラクタ.
+	 *
+	 */
+	Avatar(IGameDevice& device, ObjectManager& objectManager, Option& option, GameSceneState& gameSceneState);
+
+	/*=========================================================================*/
+	/**
+	 * @brief デストラクタ.
+	 *
+	 */
+	~Avatar();
+
+	/*=========================================================================*/
+	/**
+	 * @brief 初期化処理.
+	 * 
+	 */
+	void Initialize();
+
+	/*=========================================================================*/
+	/**
+	 * @brief 終了処理.
+	 * 
+	 */
+	void Terminate();
+
+	/*=========================================================================*/
+	/**
+	 * @brief 終了しているかどうか.
+	 * 
+	 * @return 終了フラグ.
+	 */
+	bool IsTerminated();
+
+	/*=========================================================================*/
+	/**
+	 * @brief オブジェクトの描画処理.
+	 * 
+	 */
+	void RenderObject();
+
+	/*=========================================================================*/
+	/**
+	 * @brief オブジェクトの更新処理.
+	 * 
+	 * @param[in] frameTimer 更新タイマ.
+	 */
+	void UpdateObject(float frameTimer);
+
+private:
+	/** 終了フラグ */
+	bool m_isTerminated;
+	/** ゲームデバイス */
+	IGameDevice& m_device;
+	/** オブジェクトマネージャメディエータ */
+	ObjectManager& m_objectManager;
+	/** ゲームオプション */
+	Option& m_option;
+	/** ゲームシーンステート */
+	GameSceneState m_gameSceneState;
+
+};
+
+#endif
+
+/*===== EOF ===================================================================*/
---
+++ trunk/プログラム/Game/Object/GameScene/CharacterScreen.h
@@ -0,0 +1,105 @@
+/*******************************************************************************/
+/**
+ * @file CharacterScreen.h.
+ * 
+ * @brief キャラクタースクリーンヘッダ.
+ *
+ * @date 2008/12/11.
+ *
+ * @version 1.00.
+ *
+ * @author Ryosuke Ogawa.
+ */
+/*******************************************************************************/
+#ifndef _CHARACTER_SCREEN_H_
+#define _CHARACTER_SCREEN_H_
+
+#include	"IGameDevice.h"
+#include	"Manager/Object/ObjectManager.h"
+#include	"Object/ObjectBase.h"
+#include	"Scene/GameSceneState.h"
+#include	"Manager/Scene/Option/Option.h"
+#include	"Object/GameScene/Avatar.h"
+
+
+/**
+ * @brief CharacterScreen．
+ */
+class CharacterScreen : public ObjectBase
+{
+public:
+
+	/*=========================================================================*/
+	/**
+	 * @brief コンストラクタ.
+	 * 
+	 * @param[in] device ゲームデバイス.
+	 * @param[in] objectManager オブジェクトマネージャ.
+	 * @param[in] option ゲームオプション.
+	 * @param[in] gameSceneState ゲームシーンステート.
+	 */
+	CharacterScreen(IGameDevice& device, ObjectManager& objectManager, Option& option, GameSceneState& gameSceneState);
+
+	/*=========================================================================*/
+	/**
+	 * @brief デストラクタ.
+	 *
+	 */
+	~CharacterScreen();
+
+	/*=========================================================================*/
+	/**
+	 * @brief 初期化処理.
+	 * 
+	 */
+	void Initialize();
+
+	/*=========================================================================*/
+	/**
+	 * @brief 終了処理.
+	 * 
+	 */
+	void Terminate();
+
+	/*=========================================================================*/
+	/**
+	 * @brief 終了しているかどうか.
+	 * 
+	 * @return 終了フラグ.
+	 */
+	bool IsTerminated();
+
+	/*=========================================================================*/
+	/**
+	 * @brief オブジェクトの描画処理.
+	 * 
+	 */
+	void RenderObject();
+
+	/*=========================================================================*/
+	/**
+	 * @brief オブジェクトの更新処理.
+	 * 
+	 * @param[in] frameTimer 更新タイマ.
+	 */
+	void UpdateObject(float frameTimer);
+
+private:
+	/** 終了フラグ */
+	bool m_isTerminated;
+	/** ゲームデバイス */
+	IGameDevice& m_device;
+	/** オブジェクトマネージャメディエータ */
+	ObjectManager& m_objectManager;
+	/** ゲームオプション */
+	Option& m_option;
+	/** ゲームシーンステート */
+	GameSceneState m_gameSceneState;
+
+	Avatar	m_avatar;
+
+};
+
+#endif
+
+/*===== EOF ===================================================================*/
