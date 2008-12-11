291f5f90e85dab0169082062c63da44e5b374a56
rs.drip <rs.drip@aa49b5b2-a402-11dd-98aa-2b35b7097d33>
2008-12-11 03:24:25 +0000

    Ryosuke	: アバタークラスとキャラクタースクリーンクラスを追加

---
+++ プログラム/Game/Object/GameScene/CharacterScreen.cpp
@@ -0,0 +1,93 @@
+/*******************************************************************************/
+/**
+ * @file CharacterScreen.cpp.
+ * 
+ * @brief キャラクタースクリーンソース.
+ *
+ * @date 2008/12/11.
+ *
+ * @version 1.00.
+ *
+ * @author Ryosuke Ogawa.
+ */
+/*******************************************************************************/
+
+#include	"CharacterScreen.h"
+
+/*=========================================================================*/
+/**
+ * @brief コンストラクタ.
+ */ 
+CharacterScreen::CharacterScreen(IGameDevice &device, ObjectManager &objectManager, Option &option, GameSceneState &gameSceneState) :
+	m_device(device), m_objectManager(objectManager), m_option(option), m_gameSceneState(gameSceneState), m_isTerminated(false),
+		m_avatar(device, objectManager, option, gameSceneState)
+{
+
+}
+
+/*=========================================================================*/
+/**
+ * @brief デストラクタ.
+ *
+ */
+CharacterScreen::~CharacterScreen()
+{
+
+}
+
+/*=========================================================================*/
+/**
+ * @brief 初期化処理.
+ * 
+ */
+void CharacterScreen::Initialize()
+{
+	m_avatar.Initialize();
+}
+
+/*=========================================================================*/
+/**
+ * @brief 終了処理.
+ * 
+ */
+void CharacterScreen::Terminate()
+{
+	m_avatar.Terminate();
+	m_isTerminated = true;
+}
+
+
+/*=========================================================================*/
+/**
+ * @brief 終了しているかどうか.
+ * 
+ * @return 終了フラグ.
+ */
+bool CharacterScreen::IsTerminated()
+{
+	return m_isTerminated;
+}
+
+/*=========================================================================*/
+/**
+ * @brief オブジェクトの描画処理.
+ * 
+ */
+void CharacterScreen::RenderObject()
+{
+	m_avatar.RenderObject();
+}
+
+/*=========================================================================*/
+/**
+ * @brief オブジェクトの更新処理.
+ * 
+ * @param[in] frameTimer 更新タイマ.
+ */
+void CharacterScreen::UpdateObject(float frameTimer)
+{
+	m_avatar.UpdateObject(frameTimer);
+}
+
+
+/*===== EOF ===================================================================*/
