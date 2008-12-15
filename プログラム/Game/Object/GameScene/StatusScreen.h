3dcfe0522d33125e2a24ee7b6c540ab912075648
rs.drip <rs.drip@aa49b5b2-a402-11dd-98aa-2b35b7097d33>
2008-12-15 10:37:03 +0000

    コミットすますた　チカラ.ビービーエス

--- プログラム/Game/Object/GameScene/StatusScreen.h
+++ プログラム/Game/Object/GameScene/StatusScreen.h
@@ -1,6 +1,20 @@
+/*******************************************************************************/
+/**
+ * @file StattusScreen.h.
+ * 
+ * @brief ステータススクリーンヘッダファイル.
+ *
+ * @date 2008/12/15.
+ *
+ * @version 1.00.
+ *
+ * @author Ryoma Kawasue.
+ */
+/*******************************************************************************/
 #ifndef _STATUSSCREEN_H_
 #define _STATUSSCREEN_H_
 
+/*===== インクルード ==========================================================*/
 #include	"IGameDevice.h"
 #include	"Manager/Object/ObjectManager.h"
 #include	"Object/ObjectBase.h"
@@ -8,19 +22,60 @@
 #include	"Manager/Scene/Option/Option.h"
 class		Player;
 
+/**
+ * @brief StatusScreen．
+ */
 class StatusScreen : public ObjectBase
 {
 public:
+
+	/*=========================================================================*/
+	/**
+	 * @brief コンストラクタ.
+	 * 
+	 * @param[in] device ゲームデバイス.
+	 * @param[in] objectManager オブジェクトマネージャ.
+	 * @param[in] option ゲームオプション.
+	 * @param[in] gameSceneState ゲームシーンステート.
+	 */
 	StatusScreen(IGameDevice &device, ObjectManager& objectManager, Option& option, GameSceneState& gameSceneState, Player& player);
+	/*=========================================================================*/
+	/**
+	 * @brief デストラクタ.
+	 *
+	 */
 	~StatusScreen();
+	/*=========================================================================*/
+	/**
+	 * @brief 初期化処理.
+	 * 
+	 */
 	void Initialize();
-
+	/*=========================================================================*/
+	/**
+	 * @brief 終了処理.
+	 * 
+	 */
 	void Terminate();
-
+	/*=========================================================================*/
+	/**
+	 * @brief 終了しているかどうか.
+	 * 
+	 * @return 終了フラグ.
+	 */
 	bool IsTerminated();
-
+	/*=========================================================================*/
+	/**
+	 * @brief オブジェクトの描画処理.
+	 * 
+	 */
 	void RenderObject();
-
+	/*=========================================================================*/
+	/**
+	 * @brief オブジェクトの更新処理.
+	 * 
+	 * @param[in] frameTimer 更新タイマ.
+	 */
 	void UpdateObject( float frameTimer );
 
 private:
@@ -40,3 +95,5 @@ private:
 };
 
 #endif
+
+/*===== EOF ===================================================================*/
