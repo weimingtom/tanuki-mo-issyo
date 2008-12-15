a53d6789638a32a65a0c23b932589caff1cbb78a
rs.drip <rs.drip@aa49b5b2-a402-11dd-98aa-2b35b7097d33>
2008-12-15 10:37:03 +0000

    コミットすますた　チカラ.ビービーエス

--- trunk/プログラム/Game/Object/GameScene/StatusScreen.cpp
+++ trunk/プログラム/Game/Object/GameScene/StatusScreen.cpp
@@ -1,29 +1,79 @@
+/*******************************************************************************/
+/*
+ * @file StatusScreen.cpp.
+ * 
+ * @brief ステーターススクリーンソースファイル.
+ *
+ * @date 2008/12/15.
+ *
+ * @version 1.00.
+ *
+ * @author Ryouma Kawasue.
+ */
+/*******************************************************************************/
+
+/*===== インクルード ==========================================================*/
 #include	"StatusScreen.h"
 
+/*=============================================================================*/
+/**
+ * @brief コンストラクタ.
+ * 
+ * @param[in] device ゲームデバイス.
+ * @param[in] objectManager オブジェクトマネージャ.
+ * @param[in] option ゲームオプション.
+ * @param[in] gameSceneState ゲームシーンステート.
+ */
 StatusScreen::StatusScreen(IGameDevice &device, ObjectManager &objectManager, Option &option, GameSceneState &gameSceneState, Player &player) :
 	m_device(device), m_objectManager(objectManager), m_option(option), m_gameSceneState(gameSceneState), m_player(player), m_isTerminated(false)
 {
 }
 
+/*=============================================================================*/
+/**
+ * @brief デストラクタ.
+ *
+ */
 StatusScreen::~StatusScreen()
 {
 }
 
+/*=============================================================================*/
+/**
+ * @brief 初期化処理.
+ * 
+ */
 void StatusScreen::Initialize()
 {
 
 }
 
+/*=============================================================================*/
+/**
+ * @brief 終了処理.
+ * 
+ */
 void StatusScreen::Terminate()
 {
 	m_isTerminated = true;
 }
 
+/*=============================================================================*/
+/**
+ * @brief 終了しているかどうか.
+ * 
+ * @return 終了フラグ.
+ */
 bool StatusScreen::IsTerminated()
 {
 	return m_isTerminated;
 }
 
+/*=============================================================================*/
+/**
+ * @brief オブジェクトの描画処理.
+ * 
+ */
 void StatusScreen::RenderObject()
 {
 	float m_x = 400.0f;
@@ -36,9 +86,15 @@ void StatusScreen::RenderObject()
 	m_device.GetGraphicDevice().Render( sd );
 }
 
+/*=============================================================================*/
+/**
+ * @brief オブジェクトの更新処理.
+ * 
+ * @param[in] frameTimer 更新タイマ.
+ */
 void StatusScreen::UpdateObject(float frameTimer)
 {
 
 }
 
-
+/*===== EOF ===================================================================*/
