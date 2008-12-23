c6033ff529fea986ac98a08a16e44f2b2ce0b71a
rs.drip <rs.drip@aa49b5b2-a402-11dd-98aa-2b35b7097d33>
2008-12-23 04:35:04 +0000

    Kenji	: Player位置修正

--- プログラム/Game/Scene/GameScene.cpp
+++ プログラム/Game/Scene/GameScene.cpp
@@ -33,7 +33,7 @@ GameScene::GameScene(IGameDevice& device, SceneManagerMediator& sceneManagerMedi
 	m_objectManager = new ObjectManager(device,option);
 	int skill[4] = {1,2,3,4};
 	m_objectManager->AddObject(m_objectManager->GetObjectFactory().CreatePlayer(
-		m_state,0.0f, 50.0f, 500, skill, 30, TEXTUREID_AVATAR1, 12000, 0, 5, 15, 20, TEXTUREID_SBLOCK1));
+		m_state,50.0f, 50.0f, 500, skill, 30, TEXTUREID_AVATAR1, 12000, 0, 5, 15, 20, TEXTUREID_SBLOCK1));
 
 	m_objectManager->AddObject(m_objectManager->GetObjectFactory().CreatePlayer(
 		m_state,WINDOW_WIDTH/2, 50.0f, 500, skill, 30, TEXTUREID_AVATAR2, 12000, 1, 5, 15, 20, TEXTUREID_SBLOCK1));
