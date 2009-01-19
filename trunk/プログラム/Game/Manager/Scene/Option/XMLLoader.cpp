7e934c8966e36525c4ec38b38c50df7fcd775580
rs.drip <rs.drip@aa49b5b2-a402-11dd-98aa-2b35b7097d33>
2009-01-19 09:27:38 +0000

    Tsubasa	: XMLLoader追加途中。

--- trunk/プログラム/Game/Manager/Scene/Option/XMLLoader.cpp
+++ trunk/プログラム/Game/Manager/Scene/Option/XMLLoader.cpp
@@ -16,6 +16,7 @@
 #include	<Ngl/TinyXML/XMLDomParserTinyXML.h>
 #include	<Ngl/XML/XMLUtil.h>
 #include	"XMLLoader.h"
+#include	"Define/SpriteInfo.h"
 
 /*===== íËêîêÈåæ ==============================================================*/
 
@@ -26,6 +27,37 @@
  * 
  * @param[in] à¯êîñº à¯êîê‡ñæ.
  */
+SpriteInfo LoadSpriteInfo( std::string name )
+{
+	SpriteInfo			info;
+	Ngl::XML::XMLFile	file;
+	Ngl::IXMLElement*	spriteInfo;
+	Ngl::IXMLElement*	element;
+
+	Ngl::TinyXML::XMLDomParserTinyXML xml;
+	xml.load("Resource/Data/sprite.xml", file);
+	spriteInfo = file.getElementByName("spriteInfo");
+	for(unsigned int i=0; i<spriteInfo->childElementCount(); i++)
+	{
+		if(spriteInfo->getElementByIndex(i)->getAttribByName("sprite").getText() == name)
+		{
+			element = spriteInfo->getElementByIndex(i);
+			break;
+		}
+	}
+	if(!element) return info;
+
+	info.srcPosition.x << element->getElementByName("srcPositionX")->getValue();
+	info.srcPosition.y << element->getElementByName("srcPositionY")->getValue();
+	info.srcSize.x << element->getElementByName("srcSize.x")->getValue();
+	info.srcSize.y << element->getElementByName("srcSize.y")->getValue();
+	info.position.x << element->getElementByName("position.x")->getValue();
+	info.position.y << element->getElementByName("position.y")->getValue();
+	info.size.x << element->getElementByName("size.x")->getValue();
+	info.size.y << element->getElementByName("size.y")->getValue();
+
+	return info;
+}
 
 /*=============================================================================*/
 /**
